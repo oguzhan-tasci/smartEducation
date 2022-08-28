67bb62697307b9d6ad508c9738123b8320208902
oyeProjects <oyeprojects@hotmail.com>
2022-08-28 14:16:25 +0300

    customize links

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+// Middleware
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.status(200).render('index',{
+        page_name : "index"  // degisken yolluyoruz ve bu sayede hangi link aktifse onu aktifleştiriyoruz
+    });
+})
+
+app.get('/about',(req,res) => {
+    res.status(200).render('about',{
+        page_name : "about" // degisken yolluyoruz ve bu sayede hangi link aktifse onu aktifleştiriyoruz
+    });
 })
 
 
